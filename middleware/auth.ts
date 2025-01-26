631eaccb17ed1a4cf32a5c8d429a4a95cbeb0a2c
kas <kasjunk@pm.me>
2025-01-26 19:46:59 +0100

    added logging to auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,6 +3,16 @@ import { useKeycloak } from "@josempgon/vue-keycloak";
 export default defineNuxtRouteMiddleware((to, from) => {
     const keycloak = useKeycloak();
 
+    console.log("Auth middleware", keycloak.isAuthenticated, to.path);
+    console.log("Roles", keycloak.roles);
+    console.log("Resource roles", keycloak.resourceRoles);
+    console.log("User ID", keycloak.userId);
+    console.log("Username", keycloak.username);
+    console.log("Token", keycloak.token);
+    console.log("Decoded token", keycloak.decodedToken);
+    console.log("Has roles", keycloak.hasRoles(["Data-Engineer"]));
+    console.log("Has resource roles", keycloak.hasResourceRoles(["Data-Engineer"], "Boei4"));
+    
     const protectedRoutes = [
         "/afwijkingen",
     ];
@@ -16,7 +26,6 @@ export default defineNuxtRouteMiddleware((to, from) => {
         if (!requiredRoles) {
             return true;
         }
-
         return keycloak.hasRoles(requiredRoles);
     };
 
