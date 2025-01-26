c36408880dcd2fd82de495fd0382d9f72495dd2e
kas <kasjunk@pm.me>
2025-01-26 21:18:38 +0100

    removed debug info

--- pages/afwijkingen.vue
+++ pages/afwijkingen.vue
@@ -16,18 +16,6 @@
               <strong class="pl-8">beschrijving:</strong> {{ af.beschrijving }}
             </li>
           </ul>
-
-                <!-- Debug Logs -->
-      <div class="debug-logs mt-4 p-4 bg-gray-100 rounded-lg">
-        <h2 class="text-xl font-semibold mb-2">Keycloak Debug Information</h2>
-        <p><strong>isAuthenticated:</strong> {{ isAuthenticated }}</p>
-        <p><strong>isPending:</strong> {{ isPending }}</p>
-        <p><strong>hasFailed:</strong> {{ hasFailed }}</p>
-        <p><strong>token:</strong> {{ token }}</p>
-        <p><strong>decodedToken:</strong> {{ decodedToken }}</p>
-        <p><strong>username:</strong> {{ username }}</p>
-        <p><strong>userId:</strong> {{ userId }}</p>
-        </div>
         </div>
       </div>
     </div>
@@ -35,25 +23,7 @@
 </template>
 
 <script setup>
-import { useKeycloak } from '@josempgon/vue-keycloak'
-import { ref } from 'vue'
-const {
-  // Reactive State
-  keycloak,
-  isAuthenticated,
-  isPending,
-  hasFailed,
-  token,
-  decodedToken,
-  username,
-  userId,
-  roles,
-  resourceRoles,
 
-  // Functions
-  hasRoles,
-  hasResourceRoles,
-} = useKeycloak()
 function randomSeverity() {
   const levels = ['Laag', 'Middel', 'Hoog']
   return levels[Math.floor(Math.random() * levels.length)]
@@ -75,8 +45,4 @@ const afwijkingen = ref(
     beschrijving: 'Beschrijving van de afwijking...'
   }))
 )
-
-console.error('Keycloak', keycloak, 'isAuthenticated', isAuthenticated, 'isPending', isPending, 'hasFailed', hasFailed, 'token', token, 'decodedToken', decodedToken, 'username', username, 'userId', userId, 'roles', roles, 'resourceRoles', resourceRoles);
-
-
 </script>
