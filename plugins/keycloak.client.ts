d74391a97b6812a29c29307062fa72dd7cb8b630
kas <kasjunk@pm.me>
2025-01-29 16:36:43 +0100

    added options

--- plugins/keycloak.client.ts
+++ plugins/keycloak.client.ts
@@ -1,6 +1,7 @@
 import { vueKeycloak } from '@josempgon/vue-keycloak'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const silentCheckSsoRedirectUri = `${window.location.origin}/assets/silent-check-sso.html`
   nuxtApp.vueApp.use(vueKeycloak, {
     config: {
       url: 'https://aaad01.avans.nl:8000',
@@ -8,7 +9,8 @@ export default defineNuxtPlugin((nuxtApp) => {
       clientId: 'MyApplication',
     },
     init: {
-      onLoad: 'login-required'
+      onLoad: 'check-sso',
+      silentCheckSsoRedirectUri,
     }
   })
 })
