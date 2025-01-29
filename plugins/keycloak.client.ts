afaf315dc203dc2d82dff5e262b24b7619ad8d2a
kas <kasjunk@pm.me>
2025-01-29 16:11:50 +0100

    keycloak fix

--- plugins/keycloak.client.ts
+++ plugins/keycloak.client.ts
@@ -5,7 +5,8 @@ export default defineNuxtPlugin((nuxtApp) => {
     config: {
       url: 'https://aaad01.avans.nl:8000',
       realm: 'Boei4',
-      clientId: 'MyApplication'
+      clientId: 'MyApplication',
+      redirect_uri: 'https://boei4-gqenfxb7fsa6cjg7.westeurope-01.azurewebsites.net/'
     },
     init: {
       onLoad: 'login-required'
