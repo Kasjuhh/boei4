3f5253c1cc64386e95c92c45e6f4f7f7bbeb2ccb
kas <kasjunk@pm.me>
2025-01-29 16:35:48 +0100

    removed options

--- plugins/keycloak.client.ts
+++ plugins/keycloak.client.ts
@@ -6,8 +6,6 @@ export default defineNuxtPlugin((nuxtApp) => {
       url: 'https://aaad01.avans.nl:8000',
       realm: 'Boei4',
       clientId: 'MyApplication',
-      redirect_uri: 'https://boei4-gqenfxb7fsa6cjg7.westeurope-01.azurewebsites.net/',
-      client_secret: 'FUUzkUHHfxcOepQvPBrhnHk0oukqLv1D'
     },
     init: {
       onLoad: 'login-required'
